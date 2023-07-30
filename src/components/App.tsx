e34e40bf82f29ab4040cd07d145cf628ff45bd05
Kiryl Dorakh <kirill.dorokh@gmail.com>
2023-07-30 20:18:06 +0400

    start_project

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+// import Header from "./Header"
+// import Main from "./Main";
+
+import "../styles/App.css";
+
+function App() {
+    const buttonName="update"
+
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    );
+}
+
+export default App;
